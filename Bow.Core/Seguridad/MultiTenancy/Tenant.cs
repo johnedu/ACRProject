5dda860911e5dac9a239eebbcf8d4382dc15da66
johnedu06 <johnedu06@gmail.com>
2015-05-30 21:21:07 -0500

    Antes del inicio de sesión

---
+++ Bow.Core/Seguridad/MultiTenancy/Tenant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.MultiTenancy;
+using Bow.Seguridad.Usuarios;
+using System.ComponentModel.DataAnnotations.Schema;
+using Bow.Empresas.Entidades;
+
+namespace Bow.Seguridad.MultiTenancy
+{
+    public class Tenant : AbpTenant<Tenant, User>
+    {
+        public int? OrganizacionId { get; set; }
+        public virtual Organizacion Organizacion { get; set; }
+
+        protected Tenant()
+        {
+            
+        }
+
+        public Tenant(string tenancyName, string name) : base(tenancyName, name)
+        {
+
+        }
+    }
+}
